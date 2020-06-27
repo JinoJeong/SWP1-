79877294aad666da7b28e1a3a9cb2570e8fd12cc
KMU-Jeonghj <hjjung70@kookmin.ac.kr>
2020-06-27 16:27:22 +0900

    issue#1 ADD WSGI sum and mul application

---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = b"""
+<html>
+    <body>
+        <form method="GET" action="">
+            a = <input type="number" name="a">
+            b = <input type="number" name="b"></br></br>
+            <input type="submit">
+        </form>
+        <p>
+        sum : %(sum)s</br>
+        multi : %(multi)s</br>
+        </p>
+    </body>
+</html>
+"""
